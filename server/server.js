a8533690e5a4a794a8046bcbc4064bae62dec0e0
thiagomartinssaraiva <thiagomartinssaraiva@gmail.com>
2019-11-12 00:14:00 -0300

    renderizando lista de filmes

--- server/server.js
+++ server/server.js
@@ -4,16 +4,25 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+const filmes = [
+  { nome: 'Avatar', avaliacao: '7', duracao: 170 },
+  { nome: 'Joker', avaliacao: '10', duracao: 210 },
+  { nome: 'Wolverine', avaliacao: '10', duracao: 180 },
+  { nome: 'Dance', avaliacao: '3', duracao: 120 }
+]
+
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
   res.send('Bem-vindo à locadora')
 })
 
 app.get('/filme', (req, res) => {
-  res.send('Lista de filmes')
+  res.render('filmes', { filmes })
 })
 
 app.get('/filme/:id', (req, res) => {
-  res.send(`Dados do filmes ${req.params['id']}`)
+  res.send(`Dados do filme ${req.params['id']}`)
 })
 
 app.use(cors())
