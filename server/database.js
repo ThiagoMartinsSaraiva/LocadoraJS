2557dd00d76c14c486a9526c75face5e0dcccffa
Thiago Martins Saraiva <thiagomartinssaraiva@gmail.com>
2019-11-17 23:59:05 -0300

    método de logar no backend

--- server/database.js
+++ server/database.js
@@ -51,5 +51,18 @@ module.exports = {
 		} catch(err) {
 			throw new Error(err.sqlMessage)
 		}
+	},
+	async logar(data) {
+		try {
+			const { login, senha } = data
+			let usuario = await knex('usuario').where({login, senha}).first().select()
+			if (usuario) {
+				return "Sim"
+			} else {
+				return "Não"
+			}
+		} catch(err) {
+			throw new Error(err.sqlMessage)
+		}
 	}
 }
