cd278c530b5d4b662a6b9962d0b9e22a3af516e3
ThiagoMartinsSaraiva <thiagomartinssaraiva@gmail.com>
2021-01-31 04:55:33 -0300

    convertendo LoginRepository para class

--- server/src/repositories/LoginRepository.js
+++ server/src/repositories/LoginRepository.js
@@ -1,15 +1,17 @@
-module.exports = {
+class LoginRepository {
   async logar(data) {
-		try {
-			const { login, senha } = data
-			let usuario = await knex('usuario').where({login, senha}).first().select()
-			if (usuario) {
-				return "Sim"
-			} else {
-				return "Não"
-			}
-		} catch(err) {
-			throw new Error(err.sqlMessage)
-		}
-	}
+    try {
+      const { login, senha } = data
+      const usuario = await knex('usuario').where({login, senha}).first().select()
+      if (usuario) {
+        return "Sim"
+      } else {
+        return "Não"
+      }
+    } catch(err) {
+      throw new Error(err.sqlMessage)
+    }
+  }
 }
+
+module.exports = LoginRepository
