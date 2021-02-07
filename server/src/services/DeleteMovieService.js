6564c3a4a51665d0648bd7d8fceb2b8d7ff166f0
ThiagoMartinsSaraiva <thiagomartinssaraiva@gmail.com>
2021-02-07 20:23:18 -0300

    feature: criando service de exclusão de filme

---
+++ server/src/services/DeleteMovieService.js
@@ -0,0 +1,23 @@
+const MoviesRepository = require("../repositories/MoviesRepository");
+
+class DeleteMovieService {
+  constructor() {
+    this.moviesRepository = new MoviesRepository()
+  }
+
+  async execute(id) {
+    if (id < 1) {
+      throw new Error ("Filme não encontrado!")
+    }
+
+    const [movieExist] = await this.moviesRepository.getById(id)
+
+    if (!movieExist) {
+      throw new Error ("Filme não encontrado!")
+    }
+
+    // return await this.moviesRepository.delete(id)
+  }
+}
+
+module.exports = DeleteMovieService
