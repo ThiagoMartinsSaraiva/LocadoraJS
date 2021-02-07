94663c1743233b04113f39b5c3f9339a1a10ee55
ThiagoMartinsSaraiva <thiagomartinssaraiva@gmail.com>
2021-02-07 19:20:50 -0300

    fix: conectando ao banco de dados

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -1,12 +1,10 @@
-require('dotenv').config()
-
 const knex = require('knex')({
 	client: 'mysql',
 	connection: {
-		host: process.env.DBHOST,
-		user: process.env.DBUSER,
-		password: process.env.DBPASSWORD,
-		database: process.env.DBDATABASE,
+		host: 'localhost',
+		user: 'root',
+		password: 'root',
+		database: 'locadora',
 	}
 })
 
