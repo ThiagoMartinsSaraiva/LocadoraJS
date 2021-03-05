47f713067d9869ec11b041c84e9560d254e78623
ThiagoMartinsSaraiva <thiagomartinssaraiva@gmail.com>
2021-03-05 13:17:04 -0300

    feat: adicionando links

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <h1>Bem-vindo à locadora!</h1>
+    <div>
+      <div v-if="isLoggedIn">
+        <router-link to="/movies">Movies</router-link>
+      </div>
+      <div>
+        <router-link to="/">Home</router-link>
+        <router-link to="/login">Sign in</router-link>
+      </div>
+    </div>
     <router-view></router-view>
   </div>
 </template>
